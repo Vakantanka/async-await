c2e9272bad06b78506803d57dd36280b216e8911
Vakantanka <laszlo.fenyvesi@gmail.com>
2021-12-03 12:21:25 +0100

    kész

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-function loadEvent() {
+const renderCountries = (arr) => {
+    // console.log(arr);
+    let returnHTML = "";
+    for (const country of arr) {
+        returnHTML += `<div>${country.name.official}</div>`;
+        // console.log(country.name.official);
+    }
+    return returnHTML;
+}
 
-    
+const renderFlags = (arr) => {
+    let returnHTML = "";
+    for (const flag of arr) {
+        returnHTML += `<img src="${flag.flags.png}"">`;
+        // console.log(country.name.official);
+    }
+    return returnHTML;
+}
 
-    console.log("Load completed.");
+async function loadEvent() {
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+
+    document.getElementById("root").insertAdjacentHTML("beforeend",`<div id="countryNames">${renderCountries(countryArr)}</div>`);
+    document.getElementById("root").insertAdjacentHTML("beforeend",`<div id="countryFlags">${renderFlags(countryArr)}</div>`);
+    // renderCountries(countryArr);
 }
 
 window.addEventListener("load", loadEvent);
